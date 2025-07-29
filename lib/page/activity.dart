79cc3d2b325373f26179f3758d5966dee54a9b82
RizqOsman <rizqiarab41@gmail.com>
2025-07-29 18:21:40 +0700

    gitignore

--- lib/page/activity.dart
+++ lib/page/activity.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:perisai_nusantara_app/page/components/background.dart';
 import 'package:perisai_nusantara_app/page/components/dialogactivitydetail.dart';
 import 'package:perisai_nusantara_app/page/components/dialogaddactivity.dart';
